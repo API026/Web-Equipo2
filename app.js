2c3ee2b7d8ae9afab7bf68a9c8de6812bd68ffc8
humeurRd <65732061+humeurRd@users.noreply.github.com>
2020-12-01 02:08:22 -0700

    jalando al 200+1

--- app.js
+++ app.js
@@ -1,47 +1,74 @@
-const url = 'https://apitvshows.herokuapp.com/api_tv_shows/shows/'
-fetch(url)
-    .then(respose => respose.json())
-    .then(data => {
+const url = 'https://apitvshows.herokuapp.com/api_tv_shows/shows/' //Constructor que guarda la url de nuestra API
+fetch(url) //El fetch guarda nuestra url y empezamos con las condiciones 
+    .then(respose => respose.json()) //Que nos responda con el json si es que funciona
+    .then(data => { //nos devuelve los datos del mismo
 
-        var string = "";
+        var string = ""; //declaramos variables el string guardará el nombre
         var cat = "";
-        for (let i = 0; i < 14; i++) {
+        for (let i = 0; i < 18; i++) {
             string = data.All_shows[i].name;
 
             for (let j = 0; j < string.length; j++) {
                 if (string[j] == " ") {
                     string[j] = "%20";
                 }
-
             }
+
             cat = data.All_shows[i].category;
+
             fetch('http://api.tvmaze.com/search/shows?q=' + string)
                 .then(response => response.json())
                 .then(data => {
                     let element = document.getElementById('elem')
-                    if (i == 0) {
-                        element.innerHTML += `<h3>TOP ANIMATION</h3><div class="spacer"></div>`
-                    } else if (i == 5) {
-                        element.innerHTML += `<h3>TOP DOCUMENTARY</h3><div class="spacer"></div>`
-                    } else if (i == 10) {
-                        element.innerHTML += `<h3>TOP THRILLER</h3><div class="spacer"></div>`
+                    let element1 = document.getElementById('elem1')
+                    let element2 = document.getElementById('elem2')
+
+                    if (i >=0 && i <=5) {
+                        element.innerHTML += `
+                        <article class="show-card shadow">
+                            <div class="container-display">
+                                <img src="${data[0].show.image.original}"></img>
+                                <p class="rate">Rate: ${data[0].show.rating.average}</p>    
+                            </div>
+                            <ul class = "card-cont">
+                                <h4 class= "">${data[0].show.name}</h4>
+                                <li><p>Generos: <strong>${data[0].show.genres}</strong></p></li>
+                                <br>
+                                <li><p>${data[0].show.summary}</p></li>
+                            </ul>
+                        </article>
+                        `
+                    } else if (i >=6 && i<=11) {
+                        element1.innerHTML += `
+                        <article class="show-card shadow">
+                            <div class="container-display">
+                                <img src="${data[0].show.image.original}"></img>
+                                <p class="rate">Rate: ${data[0].show.rating.average}</p>    
+                            </div>
+                            <ul class = "card-cont">
+                                <h4 class= "">${data[0].show.name}</h4>
+                                <li><p>Generos: <strong>${data[0].show.genres}</strong></p></li>
+                                <br>
+                                <li><p>${data[0].show.summary}</p></li>
+                            </ul>
+                        </article>
+                        `
+                    } else if (i >= 12 && i<=18) {
+                        element2.innerHTML += `
+                        <article class="show-card shadow">
+                            <div class="container-display">
+                                <img src="${data[0].show.image.original}"></img>
+                                <p class="rate">Rate: ${data[0].show.rating.average}</p>    
+                            </div>
+                            <ul class = "card-cont">
+                                <h4 class= "">${data[0].show.name}</h4>
+                                <li><p>Generos: <strong>${data[0].show.genres}</strong></p></li>
+                                <br>
+                                <li><p>${data[0].show.summary}</p></li>
+                            </ul>
+                        </article>
+                        `
                     }
-
-
-                    element.innerHTML += `
-                    <article class="show-card shadow">
-                        <div class="container-display">
-                            <img src="${data[0].show.image.original}"></img>
-                            <p class="rate">Rate: ${data[0].show.rating.average}</p>    
-                        </div>
-                        <ul class = "card-cont">
-                            <h4 class= "">${data[0].show.name}</h4>
-                            <li><p>Generos: <strong>${data[0].show.genres}</strong></p></li>
-                            <br>
-                            <li><p>${data[0].show.summary}</p></li>
-                        </ul>
-                    </article>
-                    `
 
                     console.log(data)
                 })
