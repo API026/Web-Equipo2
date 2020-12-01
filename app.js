9c0588d97a71a1cba06bfee490bdbca4b46b9934
artoria026 <artoria026@protonmail.com>
2020-11-30 20:20:32 -0700

    aaaaaaa

--- app.js
+++ app.js
@@ -11,10 +11,10 @@
                 element.innerHTML += `<article class="show-card shadow">
                 <img src="${data[i].image.original}"></img>
                 <ul class = "card-cont">
-                    <div class="aux-list-cont">
+                    
                         <h4 class= "">${data[i].name}</h4>
                         <p class = "description ">Rate: ${data[i].rating.average}</p>    
-                    </div>
+                
                     <li><p class = "description ">País: ${data[i].network.country.name}</p></li>
                     <li><p class = "description ">Canal de transmición: ${data[i].network.name}</p></li>
                     <li><p class = "description ">Generos: ${data[i].genres}</p></li>
