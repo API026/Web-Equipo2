457c1259259b4a8b0ed811a4d920843c97b1f689
ShiWonWan <emmanuelcampu@gmail.com>
2020-11-30 21:36:11 -0700

    accepted

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 
 const url = 'http://api.tvmaze.com/shows';
 
-<<<<<<< HEAD
     fetch(url)
         .then(Response => Response.json())
         .then(data =>{
@@ -30,34 +29,5 @@ const url = 'http://api.tvmaze.com/shows';
         })
         .catch(err=>console.log(err))
         
-=======
-fetch(url)
-    .then(Response => Response.json())
-    .then(data => {
-
-        let element = document.getElementById('elem')
-        for (let i = 0; i < 239; i++) {
-
-            element.innerHTML += `<article class="show-card shadow">
-                <div class="container-display">
-                    <img src="${data[i].image.original}"></img>
-                    <p class="rate">Rate: ${data[i].rating.average}</p>    
-                </div>
-                <ul class = "card-cont">
-                    <h4 class= "">${data[i].name}</h4>
-                    <li><p>País: <strong>${data[i].network.country.name}</strong></p></li>
-                    <li><p>Canal de transmición: <strong>${data[i].network.name}</strong></p></li>
-                    <li><p>Generos: <strong>${data[i].genres}</strong></p></li>
-                    <br>
-                    <li><p>${data[i].summary}</p></li>
-                </ul>
-                </article>
-                `
-        }
-        console.log(data)
-    })
-    .catch(err => console.log(err))
-
->>>>>>> d7ca75496873a55a31002838543e00e802cd35da
 
 
