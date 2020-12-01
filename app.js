3c4666fcaaad1632a1a534dbf4c9b5efdd00f77f
artoria026 <artoria026@protonmail.com>
2020-11-30 21:11:14 -0700

    listo listo

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
 
-    const url = 'http://api.tvmaze.com/shows';
+const url = 'http://api.tvmaze.com/shows';
 
-    fetch(url)
-        .then(Response => Response.json())
-        .then(data =>{
-    
-            let element = document.getElementById('elem')
-            for(let i = 0; i<239; i++){
-               
-                element.innerHTML += `<article class="show-card shadow">
-                <img src="${data[i].image.original}"></img>
+fetch(url)
+    .then(Response => Response.json())
+    .then(data => {
+
+        let element = document.getElementById('elem')
+        for (let i = 0; i < 239; i++) {
+
+            element.innerHTML += `<article class="show-card shadow">
+                <div class="container-display">
+                    <img src="${data[i].image.original}"></img>
+                    <p class="rate">Rate: ${data[i].rating.average}</p>    
+                </div>
                 <ul class = "card-cont">
-                    
-                        <h4 class= "">${data[i].name}</h4>
-                        <p class = "description ">Rate: ${data[i].rating.average}</p>    
-                
-                    <li><p class = "description ">País: ${data[i].network.country.name}</p></li>
-                    <li><p class = "description ">Canal de transmición: ${data[i].network.name}</p></li>
-                    <li><p class = "description ">Generos: ${data[i].genres}</p></li>
-                    <li><p class = "description ">Descrpción: ${data[i].summary}</p></li>
+                    <h4 class= "">${data[i].name}</h4>
+                    <li><p>País: ${data[i].network.country.name}</p></li>
+                    <li><p>Canal de transmición: ${data[i].network.name}</p></li>
+                    <li><p>Generos: ${data[i].genres}</p></li>
+                    <br>
+                    <li><p>Descrpción: ${data[i].summary}</p></li>
                 </ul>
                 </article>
                 `
-            }
-            console.log(data)
-        })
-        .catch(err=>console.log(err))
-        
+        }
+        console.log(data)
+    })
+    .catch(err => console.log(err))
+
 
 
