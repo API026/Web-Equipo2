ca839dd1117420bb4d276fcbcf4286976a9da130
humeurRd <65732061+humeurRd@users.noreply.github.com>
2020-12-01 01:00:39 -0700

    enlace entre nuestra api y la externa

--- app.js
+++ app.js
@@ -1,32 +1,49 @@
-
-const url = 'http://api.tvmaze.com/shows';
-
-    fetch(url)
-        .then(Response => Response.json())
-        .then(data =>{
-    
-            let element = document.getElementById('elem')
-            for(let i = 0; i<239; i++){
-                if(data[i].image.original){
-                    element.innerHTML += `<article class="show-card shadow">
-                <div class="container-display">
-                    <img src="${data[i].image.original}"></img>
-                    <p class="rate">Rate: ${data[i].rating.average}</p>    
-                </div>
-                <ul class = "card-cont">
-                    <h4 class= "">${data[i].name}</h4>
-                    <li><p>País: <strong>${data[i].network.country.name}</strong></p></li>
-                    <li><p>Canal de transmición: <strong>${data[i].network.name}</strong></p></li>
-                    <li><p>Generos: <strong>${data[i].genres}</strong></p></li>
-                    <br>
-                    <li><p>${data[i].summary}</p></li>
-                </ul>
-                </article>
-                `
-                }
-                
+const url = 'https://apitvshows.herokuapp.com/api_tv_shows/shows/'
+fetch(url)
+.then(respose => respose.json())
+.then(data =>{
+  
+    var string = "";
+    var cat = "";
+    for(let i = 0; i<14; i++){
+        string = data.All_shows[i].name;
+       
+        for(let j = 0; j<string.length; j++){
+            if(string[j] == " "){
+                string[j] = "%20";
             }
+            
+        }
+        cat = data.All_shows[i].category;
+        fetch('http://api.tvmaze.com/search/shows?q='+string)
+        .then(response => response.json())
+        .then(data=>{
+            let element = document.getElementById('elem')
+            if(i == 0){
+                element.innerHTML +=`<h2>TOP ANIMATION</h2><div class="spacer"></div>`
+            }else if(i == 5){
+                element.innerHTML +=`<h2>TOP DOCUMENTARY</h2><div class="spacer"></div>`
+            }else if(i==10){
+                element.innerHTML +=`<h2>TOP THRILLER</h2><div class="spacer"></div>`
+            }
+            element.innerHTML += `<article class="show-card shadow">
+        <div class="container-display">
+            <img src="${data[0].show.image.original}"></img>
+            <p class="rate">Rate: ${data[0].show.rating.average}</p>    
+        </div>
+        <ul class = "card-cont">
+            <h4 class= "">${data[0].show.name}</h4>
+            <li><p>Generos: <strong>${data[0].show.genres}</strong></p></li>
+            <br>
+            <li><p>${data[0].show.summary}</p></li>
+        </ul>
+        </article>`
+        
             console.log(data)
         })
-        .catch(err=>console.log(err))
-  
+        .catch(err => console.log(err))
+    }
+    
+    console.log(data)
+})
+.catch(err=>console.log(err))
